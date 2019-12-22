e6c1de47a80bdfbd2b92296a4c1eb832992c50cc
rightstartwp <moshe@rightstartwp.com>
2019-12-22 15:19:50 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Ant Design Starter`,
+    description: `Gatsby Starter featuring Ant Design, Styled Components and an opinionated file structure.`,
+    author: `moshemo`,
   },
   plugins: [
+    // PLUGINS
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,8 +21,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+
+    // SOURCES
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    // TRANSFORMERS
+    `gatsby-transformer-sharp`,
   ],
 }
